bb459444854431ab172ac17df0333e3a9bf701e3
Dan <darkepacific@gmail.com>
2017-02-22 20:01:43 -0800

    Update ArrivalEvent.java

--- src/ArrivalEvent.java
+++ src/ArrivalEvent.java
@@ -1,23 +1,37 @@
 
 public class ArrivalEvent extends Event {
-	private int processingTime = 0;
+	private double arrivalTime = 0;
 	
-	public ArrivalEvent(int eventTime){
-		//set random times here
+	public ArrivalEvent(double currTime){
+		setArrivalTime(currTime + randArrival());
+		
 	}
 	
-	public ArrivalEvent(int eventTime, int processingTime){
+	public ArrivalEvent(double currTime, double randTime){
 		//set random times here
-		setProcessingTime(processingTime);
+		
+		
+		setArrivalTime(double ArrivalTime);
 	}
 
 	
-	public int getProcessingTime(){
-		return processingTime;
+	public int getArrivalTime(){
+		return arrivalTime;
 	}
 	
-	public void setProcessingTime(int time){
-		processingTime = time;
+	public void setArrivalTime(double time){
+		arrivalTime = time;
+	}
+	
+	
+	private double randArrival{
+		/*double negative-exponenetially-distributed-time(double rate){
+     		double u;
+     		u = drand48();
+     		return ((-1/rate)*log(1-u));*/
+		
+		double u = 0;
+		return u;
 	}
 
 }
