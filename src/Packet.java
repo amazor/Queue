08489daae4fc0211f04841fdf7f699f406c9b4da
Dan <darkepacific@gmail.com>
2017-02-22 21:22:01 -0800

    Update Packet.java

--- src/Packet.java
+++ src/Packet.java
@@ -1,9 +1,19 @@
+import java.util.Random;
+
 
 public class Packet {
 	private double serviceTime;
 	
+	/** Create Random Distribution**/
+	private static Random packetRand = new Random();
+	double mu = 1;
+
 	
-	public Packet (){}
+	public Packet (){
+	double u = packetRan.nextDouble();
+	serviceTime = ((-1/mu)*Math.log(1-u)); 
+		
+	}
 
 	public Packet (double serviceTime){
 		this.serviceTime = serviceTime;
