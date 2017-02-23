202e89fdb500ee21c8b2f138174164f6f29a0401
Dan <darkepacific@gmail.com>
2017-02-22 20:12:13 -0800

    Update DepartureEvent.java

--- src/DepartureEvent.java
+++ src/DepartureEvent.java
@@ -1,8 +1,29 @@
 
 public class DepartureEvent extends Event{
+	//private double departTime = 0;
 	
-	public DepartureEvent(int time){
-		setEventTime(time);
+	public DepartureEvent(double currTime){
+		setDepartTime(currTime + randDepart());
 	}
+	
+	public void setDepartTime(double time){
+		eventTime = time;
+	}
+	
 
+	public int getDepartTime(){
+		return eventTime;
+	}
+	
+	private double randDepart{
+		/*double negative-exponenetially-distributed-time(double rate){
+     		double u;
+     		u = drand48();
+     		return ((-1/rate)*log(1-u));*/
+		
+		double u = 0;
+		return u;
+	}
+	
+	
 }
