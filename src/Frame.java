ae28eba5ba912ac903ba671fcdfa0be0808bbeac
amazor <amirmazorsonny@gmail.com>
2017-03-12 22:48:31 -0700

    added Dan's changes

--- src/Frame.java
+++ src/Frame.java
@@ -44,3 +44,4 @@ public class Frame extends Packet {
 	}
 }
 	
+
--- src/Host.java
+++ src/Host.java
@@ -1,12 +1,14 @@
 import mm1.*;
 
+import java.util.Queue;
+import java.util.Random;
+
 public class Host {
 	int repetitionCount; //keeps track if there is repetition, and numOfTimeOuts in current Pkt
 	boolean isDIFS;
 	boolean isBackOff;
 	private boolean isWaitingTimeout;
 	
-
 	int DIFSCounter = 0;		//DIFS to TICKS
  	int BackCounter = 0;
 	private static int T = 5;    //Vary T in the random backoff algorithm to find out the effect of T on throughput and average network delay by keeping λ constant.
@@ -18,7 +20,25 @@ public class Host {
 	private Buffer buff;
 	private Buffer ackBuffer;
 	
+	int tickCounter = 0;
+	int ptr = 0;
+	private Queue<Packet> arrivals;
+	private double[] arrivalEvents;
+	private static Random rand = new Random();
+	
 	public void tick() throws BufferOutOfBoundsException{
+		tickCounter ++;
+		int ITSOWNNUMBER = 0;
+		
+		//Adds an arrival event to the Buffer
+		if(arrivalEvents[ptr] == tickCounter){
+			Frame theFrame = new Frame(ITSOWNNUMBER,(tickCounter % Main.NUM_HOSTS) );
+			buff.increment(theFrame);
+			ptr++;
+		}
+		
+		
+		
 		if(isDIFS){ // is it waiting on DIFS
 			DIFSCounter--;
 			if (DIFSCounter == 0){
@@ -71,7 +91,21 @@ public class Host {
 		ackBuffer = new Buffer(-1);
 		sharedBus = bus;
 		repetitionCount = 0;
+		
+		//Made it an array after all, so we could have a record of when items were added to the Buffer
+		arrivalEvents = new double[(int) (1.5*Main.SIM_TIME)];    // The size of this array determines the total number of Events that will ever occur
+		arrivalEvents[0] = randomArrival()*1000/Main.DEFINITION;
+		for(int index = 1; index < arrivalEvents.length; index++){ 
+			arrivalEvents[index] = arrivalEvents[index-1] + randomArrival();
+		}	
 	}
+	
+	private static double randomArrival(){
+		double lambda = .9;        //As Lambda increases the time between arrivals decreases
+		double u = rand.nextDouble();
+		return ((-1/lambda)*Math.log(1-u));
+	}
+	
 	
 
 	public boolean isFrameToSend(){
