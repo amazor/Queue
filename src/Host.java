785f3b3e25eb9b87738e1c99f3078f2875a45e65
Dan <darkepacific@gmail.com>
2017-03-12 21:30:54 -0700

    Update Host.java

--- src/Host.java
+++ src/Host.java
@@ -6,7 +6,6 @@ public class Host {
 	boolean isBackOff;
 	private boolean isWaitingTimeout;
 	
-
 	int DIFSCounter = 0;		//DIFS to TICKS
  	int BackCounter = 0;
 	private static int T = 5;    //Vary T in the random backoff algorithm to find out the effect of T on throughput and average network delay by keeping λ constant.
@@ -15,11 +14,29 @@ public class Host {
 	private int timeoutCounter = 0;  //Set this to Largest Pckt + DIFS + SIFS   
 	
 	private Bus sharedBus;
-	private GEL gel;
 	private Buffer buff;
 	private Buffer ackBuffer;
 	
+	int tickCounter = 0;
+	int ptr = 0;
+	private Queue<Packet> arrivals;
+	private double[] arrivalEvents;
+	private Random rand = new Random();
+	
 	public void tick() throws BufferOutOfBoundsException{
+		tickCounter ++;
+		int ITSOWNNUMBER = 0;
+		
+		//Adds an arrival event to the Buffer
+		if(arrivalEvents[ptr] == tickCounter){
+			Frame theFrame = new Frame(ITSOWNNUMBER,(tickCounter % NUM_HOSTS) );
+			buff.increment(theFrame);
+			ptr++;
+		
+		}
+		
+		
+		
 		if(isDIFS){ // is it waiting on DIFS
 			DIFSCounter--;
 			if (DIFSCounter == 0){
@@ -72,6 +89,13 @@ public class Host {
 		ackBuffer = new Buffer(-1);
 		sharedBus = bus;
 		repetitionCount = 0;
+		
+		//Made it an array after all, so we could have a record of when items were added to the Buffer
+		arrivalEvents = new double[40];    // The size of this array determines the total number of Events that will ever occur
+		arrivalEvents[0] = randArrival();
+		for(int index = 1; index < arrivalEvents.length; index++){ 
+			arrivalEvents[index] = arrivalEvents[index-1] + randArrival();
+		}	
 	}
 	
 
