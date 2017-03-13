4de8530ae563d5d9eae0cb977e5c145db9cc614e
Dan <darkepacific@gmail.com>
2017-03-12 18:04:14 -0700

    Update Frame.java

--- src/Frame.java
+++ src/Frame.java
@@ -1,16 +1,19 @@
 import mm1.Packet;
+import java.util.Random;
 
 public class Frame extends Packet {
 	private int srcHost;
 	private int destHost;
-	private int size;
 	private int FrameIDNum;
+	private int size;
+	private static Random randF = new Random();
 	
 	public Frame(int srcHostID, int DestHostID){
 		super();
 		srcHost = srcHostID;
 		destHost = DestHostID;
 		//randomsize	
+		size = randomSize();	
 	}
 	public void setSize(int newSize){
 		size = newSize;
@@ -29,5 +32,12 @@ public class Frame extends Packet {
 	public String toString(){
 		return "Src:" + srcHost + " Dest: " + destHost;
 	}
+	
+	private int randomSize(){
+		double lambda = .1;    
+		double u = randF.nextDouble();
+		return (int)((-1/lambda)*Math.log(1-u)) + 64;  // The 64 is just there as a base so it's larger than an ACK
+	}
+	
 
 }
