62f0eb61997825b0116fed4ca3a884605f3a0109
Dan <darkepacific@gmail.com>
2017-03-12 18:27:29 -0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
 import mm1.BufferOutOfBoundsException;
 
 public class Main {
-	public static final double DEFINITION = 0.01; //must be <= 0.01 in ms
+	public static final double DEFINITION = 0.01; //must be <= 0.01 in ms  Defines the size of a Tick
 	public static final int NUM_HOSTS = 10;
 	public static final double LAMBDA = 0.1;
 	public static final double SIM_TIME = 100; //in seconds
 	public static final double SPEED = 11; //in mbps
 	
 	public static int globalTime;
-	public static double conversionFactor;
+	public static double conversionFactor = 1/DEFINITION;;
 	public static double endTime;
 	public static WiFiSystem wifi;
 	
@@ -29,7 +29,6 @@ public class Main {
 	private static void init() {
 		globalTime = 0;
 		wifi = new WiFiSystem(NUM_HOSTS, SPEED);
-		conversionFactor = 1/DEFINITION;
 		endTime =  conversionFactor * SIM_TIME/1000;
 		}
 
