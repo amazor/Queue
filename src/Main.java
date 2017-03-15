b0591b972872d51ec6f8411d60889abab1f7f285
Dan <darkepacific@gmail.com>
2017-03-15 10:50:36 -0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,8 @@ import mm1.BufferOutOfBoundsException;
 public class Main {
 	public static final double DEFINITION = 0.01; //must be <= 0.01 in ms  Defines the size of a Tick
 	public static final int NUM_HOSTS = 10;
-	public static final double LAMBDA = 0.9;
+	public static final double LAMBDA_Arrive = 0.9;
+	public static final double LAMBDA_SIZE = 0.9;
 	public static final double SIM_TIME = 10; //in seconds
 	public static final double SPEED = 11; //in mbps
 	
